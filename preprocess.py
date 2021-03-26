8f55f648c1dc7d7ec4c7939c7178ea20f91a8026
tommy <tommywhy1989@gmail.com>
2021-03-26 22:44:04 +1300

    testing done

--- preprocess.py
+++ preprocess.py
@@ -43,6 +43,7 @@ def load_data(args):
         ])
 
         transform_test = transforms.Compose([
+
             transforms.Resize(224),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
